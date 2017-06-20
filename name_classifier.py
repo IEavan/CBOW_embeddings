4cdbc46ff6deb73c96d0a3a024f4831c6d3f1e36
Eavan Pattie <eavan@lyse.net>
2017-06-21 00:56:11 +0100

    added helper methods and model

--- name_classifier.py
+++ name_classifier.py
@@ -2,9 +2,13 @@ import glob
 import unicodedata
 import string
 
+import torch
+from torch.autograd import Variable
+
 # Constants
 allowed_chars = string.ascii_letters + " .,;'"
 n_chars = len(allowed_chars)
+n_hidden = 128
 
 # Converts unicode strings into strings containing only ascii
 # Characters. Accented letters will have their accents
@@ -24,8 +28,8 @@ def find_files(path):
 # returns a list of the lines in ascii format
 def read_lines(filename):
     with open(filename, encoding="UTF-8") as lines:
-        lines = lines.read().strip().split('/n')
-        return [unicode_to_ascii(line) for line in lines]
+        names = lines.read().strip().split('\n')
+        return [unicode_to_ascii(name) for name in names]
 
 # Declare data containers
 category_lines = {}
@@ -39,3 +43,49 @@ for filename in find_files("name_data/names/*.txt"):
     category_lines[category] = lines
 
 n_categories = len(categories)
+
+# Helper methods for converting strings to onehot tensors
+def letter_to_variable(letter):
+    one_hot_tensor = torch.zeros(1, n_chars)
+    index = allowed_chars.find(letter)
+    one_hot_tensor[0][index] = 1
+    return Variable(one_hot_tensor)
+
+def name_to_variable(name):
+    one_hot_tensor = torch.zeros(len(name), 1, n_chars)
+    # Loop over and add ones at a corresponding index
+    for i in range(len(name)):
+        index = allowed_chars.find(name[i])
+        one_hot_tensor[i][0][index] = 1
+    return Variable(one_hot_tensor)
+
+
+#
+# Define RNN Model
+class RNN(torch.nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(RNN, self).__init__()
+        self.hidden_size = hidden_size
+        
+        # parameters
+        c2h = torch.nn.Linear(input_size + hidden_size, hidden_size)
+        c2o = torch.nn.Linear(input_size + hidden_size, output_size)
+
+        softmax = torch.nn.LogSoftmax()
+
+    def forward(self, letter, hidden):
+       combinded = torch.cat((letter, hidden), 1) 
+       
+       next_hidden = self.c2h(combinded)
+       output = self.c2o(combinded)
+       output = self.softmax(output)
+       return output, next_hidden
+
+   def init_hidden():
+       return Variable(torch.zeros(1, self.hidden_size))
+
+
+#____________________________________________________________
+
+model = RNN(n_chars, n_hidden, n_catagory)
+
