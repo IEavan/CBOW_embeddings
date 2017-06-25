3d02716500341c30ef115c46daecd0546b634ac6
Eavan Pattie <eavan@lyse.net>
2017-06-25 20:15:44 +0100

    finished proof of concept for training name classifier

--- name_classifier.py
+++ name_classifier.py
@@ -1,6 +1,7 @@
 import glob
 import unicodedata
 import string
+import random
 
 import torch
 from torch.autograd import Variable
@@ -59,6 +60,10 @@ def name_to_variable(name):
         one_hot_tensor[i][0][index] = 1
     return Variable(one_hot_tensor)
 
+def probs_to_category(probs):
+    probability, index = torch.max(probs, 1)
+    category = categories[index.data[0][0]]
+    return category, index.data[0][0]
 
 #
 # Define RNN Model
@@ -68,10 +73,10 @@ class RNN(torch.nn.Module):
         self.hidden_size = hidden_size
         
         # parameters
-        c2h = torch.nn.Linear(input_size + hidden_size, hidden_size)
-        c2o = torch.nn.Linear(input_size + hidden_size, output_size)
+        self.c2h = torch.nn.Linear(input_size + hidden_size, hidden_size)
+        self.c2o = torch.nn.Linear(input_size + hidden_size, output_size)
 
-        softmax = torch.nn.LogSoftmax()
+        self.softmax = torch.nn.LogSoftmax()
 
     def forward(self, letter, hidden):
        combinded = torch.cat((letter, hidden), 1) 
@@ -81,11 +86,53 @@ class RNN(torch.nn.Module):
        output = self.softmax(output)
        return output, next_hidden
 
-   def init_hidden():
+    def init_hidden(self):
        return Variable(torch.zeros(1, self.hidden_size))
 
 
 #____________________________________________________________
 
-model = RNN(n_chars, n_hidden, n_catagory)
+model = RNN(n_chars, n_hidden, n_categories)
 
+# name = "Johnny"
+# var = name_to_variable(name)
+# hidden = model.init_hidden()
+# out, next_hidden = model(var[0], hidden)
+# print(out)
+# print(probs_to_category(out))
+
+def get_random_example():
+   rand_category_index = random.randint(0, n_categories - 1)
+   category = categories[rand_category_index]
+   rand_line_index = random.randint(0, len(category) - 1)
+   line = category_lines[category][rand_line_index]
+   return line, category, rand_category_index
+
+criterion = torch.nn.NLLLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.0005)
+
+def train():
+    line, category, category_index = get_random_example()
+    line = name_to_variable(line)
+    
+    optimizer.zero_grad()
+    hidden = model.init_hidden()
+
+    for i in range(line.size()[0]):
+        out, hidden = model(line[i], hidden)
+
+    predicted_category, p_cat_index = probs_to_category(out)
+
+    loss = criterion(out, Variable(torch.LongTensor([category_index]))) 
+    loss.backward()
+    optimizer.step()
+
+    return predicted_category, loss.data[0]
+
+for epoch in range(100):
+    error = 0
+    for i in range(100):
+        _, err = train()
+        error += err
+
+    print(error / 100)
