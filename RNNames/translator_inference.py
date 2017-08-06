00fc10a20787a7ee4494ac51b0080f22ef2340b8
Eavan Pattie <eavan@lyse.net>
2017-08-06 23:18:04 +0200

    added interative functionality to the model

--- RNNames/translator_inference.py
+++ RNNames/translator_inference.py
@@ -18,8 +18,6 @@ class Model():
         to translate a string from the input_lang to the output_lang
         provided that the encoder and decoder are trained on that pair"""
 
-        # TODO: replace encoder, decoder and langs with self
-
         tokenized = tc.normalize_string(sentence)
         input_variable = tc.sentence_to_variable(tokenized, self.input_lang)
 
@@ -58,6 +56,37 @@ class Model():
         return result
 
 if __name__ == "__main__":
-    # TODO: write code for interactive translations
+
+    print("Loading model...")
     model = Model()
-    print(model.translate("She is hungry"))
+
+    print("Model Loaded, enter a phrase to translate, use q to quit")
+    print("Input phrases must start with (s)he/you/I/they/it is/are/am")
+    print("and be no longer than ten words")
+
+    while True:
+        english_text = input("\nInput:  ")
+
+        if english_text is not 'q':
+            # Check to make sure english_text is translatable by the model
+
+            english_text = tc.normalize_string(english_text)
+            valid = True
+            if not tc.is_simple((english_text, "")):
+                print("Input is either too long or")
+                print("does not start with (s)he/you/I/they/it is/are/am")
+                valid = False
+
+            for word in tc.normalize_string(english_text).split(" "):
+                if word not in model.input_lang.word2index:
+                    print("The word '{}' was not recognized by the model".format(word))
+                    print("Please try again")
+                    valid = False
+
+            if not valid:
+                continue
+
+            print("Output: {}".format(model.translate(english_text)))
+        else:
+            print("Goodbye")
+            break
